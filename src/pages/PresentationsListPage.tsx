0b14355ca04225873ad5a8480b2ad3b5176242eb
bwestlund17 <94927884+bwestlund17@users.noreply.github.com>
2025-06-02 16:32:45 -0500

    Fix presentations ordering with related tables

--- src/pages/PresentationsListPage.tsx
+++ src/pages/PresentationsListPage.tsx
@@ -48,7 +48,7 @@ const PresentationsListPage: React.FC = () => {
             industry
           )
         `)
-        .order('companies.name', { ascending: true })
+        .order('companies!fk_company_symbol(name)', { ascending: true })
         .range((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE - 1);
 
       if (selectedIndustries.length > 0) {
