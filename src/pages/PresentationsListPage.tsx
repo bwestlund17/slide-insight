eb7f857e628ec079d58d725c39fc100e02e1d529
bwestlund17 <94927884+bwestlund17@users.noreply.github.com>
2025-06-02 16:31:22 -0500

    Fix Supabase order clause syntax

--- src/pages/PresentationsListPage.tsx
+++ src/pages/PresentationsListPage.tsx
@@ -48,7 +48,7 @@ const PresentationsListPage: React.FC = () => {
             industry
           )
         `)
-        .order('companies.name')
+        .order('companies.name', { ascending: true })
         .range((currentPage - 1) * ITEMS_PER_PAGE, currentPage * ITEMS_PER_PAGE - 1);
 
       if (selectedIndustries.length > 0) {
